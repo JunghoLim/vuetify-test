fe20fd9a38bde3021095bca99e7db8cfcf4a47e7
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-07-22 17:35:58 +0900

    add vuetify grid and classes error setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: [
-    'plugin:vue/recommended',
-  ],
-  plugins: [
-    'vuetify'
-  ],
-  rules: {
-    'vuetify/no-deprecated-classes': 'error'
-  }
+    extends: [
+        'plugin:vue/recommended',
+    ],
+    plugins: [
+        'vuetify'
+    ],
+    rules: {
+        'vuetify/no-deprecated-classes': 'error',
+        'vuetify/no-legacy-grid': 'error'
+    }
 }
