ce90c1af069c5a9059f3b2de98a81d823ed08659
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-07-23 18:28:16 +0900

    컴포넌트 분리 및 transition event 효과 추가

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,11 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-spacer />
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      dark
-      app
-      :src="require('@/assets/sidebar.png')"
-    >
-      <template #img="props">
-        <v-img :gradient="gradient" v-bind="props"> </v-img>
-      </template>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6"> Application </v-list-item-title>
-          <v-list-item-subtitle> subtext </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider />
-
-      <v-list dense nav>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          :to="item.to"
-          active-class="primary"
-          class="py-1"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <v-container fluid>
-        <router-view />
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-fade-transition mode="out-in">
+    <router-view />
+  </v-fade-transition>
 </template>
 
 <script>
 export default {
   name: "App",
-
-  data() {
-    return {
-      items: [
-        { title: "Dashboard", icon: "mdi-view-dashboard", to: "/" },
-        { title: "GridSystem", icon: "mdi-image", to: "/grid-system" },
-        { title: "GridListPage", icon: "mdi-image", to: "/grid-list-page" },
-        { title: "Breakpoints", icon: "mdi-image", to: "/break-points" },
-        { title: "Tables", icon: "mdi-table-settings", to: "/tables" },
-        { title: "Forms", icon: "mdi-form-select", to: "/forms" },
-        { title: "Buttons", icon: "mdi-gesture-tap-button", to: "/buttons" },
-        { title: "Icons", icon: "mdi-emoticon-excited-outline", to: "/icons" },
-      ],
-      drawer: false,
-      right: null,
-      gradient: "rgba(0,0,0,.7), rgba(0,0,0,.7)",
-    };
-  },
 };
 </script>
