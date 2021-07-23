00cf0cfe1d1c192629899be16036a5970b3a3528
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-07-23 18:29:23 +0900

    로그인, 로그아웃,상품목록,레이아웃 라우트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -8,48 +8,83 @@ import Tables from '@/views/Tables'
 import Forms from '@/views/Forms'
 import Buttons from '@/views/Buttons'
 import Icons from '@/views/Icons'
+import SignIn from '@/views/authentication/SignIn'
+import SignUp from '@/views/authentication/SignUp'
+import ProductList from '@/views/page/ProductList'
+import DefaultLayout from '@/layouts/default/index'
+import AuthenticationLayout from '@/layouts/authentication/index'
+import PageLayout from '@/layouts/page/index'
 
 Vue.use(VueRouter)
 
 const routes = [{
         path: '/',
-        name: 'DashBoard',
-        component: Dashboard
+        component: DefaultLayout,
+        children: [{
+                path: '/',
+                name: 'DashBoard',
+                component: Dashboard
+            },
+            {
+                path: '/grid-system',
+                name: 'GridSystem',
+                component: GridSystem
+            },
+            {
+                path: '/grid-list-page',
+                name: 'GridListPage',
+                component: GridListPage
+            },
+            {
+                path: '/break-points',
+                name: 'Breakpoints',
+                component: Breakpoints
+            },
+            {
+                path: '/tables',
+                name: 'Tables',
+                component: Tables
+            },
+            {
+                path: '/buttons',
+                name: 'Buttons',
+                component: Buttons
+            },
+            {
+                path: '/forms',
+                name: 'Forms',
+                component: Forms
+            },
+            {
+                path: '/icons',
+                name: 'Icons',
+                component: Icons
+            }
+        ]
     },
     {
-        path: '/grid-system',
-        name: 'GridSystem',
-        component: GridSystem
+        path: '/authentication',
+        component: AuthenticationLayout,
+        children: [{
+                path: 'sign-in',
+                name: 'SignIn',
+                component: SignIn
+            },
+            {
+                path: 'sign-up',
+                name: 'SignUp',
+                component: SignUp
+            }
+        ]
     },
     {
-        path: '/grid-list-page',
-        name: 'GridListPage',
-        component: GridListPage
-    },
-    {
-        path: '/break-points',
-        name: 'Breakpoints',
-        component: Breakpoints
-    },
-    {
-        path: '/tables',
-        name: 'Tables',
-        component: Tables
-    },
-    {
-        path: '/buttons',
-        name: 'Buttons',
-        component: Buttons
-    },
-    {
-        path: '/forms',
-        name: 'Forms',
-        component: Forms
-    },
-    {
-        path: '/icons',
-        name: 'Icons',
-        component: Icons
+        path: '/page',
+        component: PageLayout,
+        children: [{
+            path: 'product-list',
+            name: 'ProductList',
+            component: ProductList
+        }]
     }
 ]
 
