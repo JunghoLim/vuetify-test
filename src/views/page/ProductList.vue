a48b9d46104bb010768bfa1ab5d2382186d2dc60
JunghoLim <77674000+JunghoLim@users.noreply.github.com>
2021-07-23 18:34:36 +0900

    product page view

---
+++ src/views/page/ProductList.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid app>
+    <v-row>
+      <v-col v-for="item in 20" :key="item" cols="12" sm="5" md="4" lg="3">
+        <product-item />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ProductItem from "../../components/product/ProductItem";
+export default {
+  name: "ProductList",
+  components: { ProductItem },
+};
+</script>
+
+<style>
+</style>
