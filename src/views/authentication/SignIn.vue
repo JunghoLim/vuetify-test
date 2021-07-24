96f8325096ff08b5b0c9137aa9f3822a9ef94f79
JungHoLim <limushman777@gmail.com>
2021-07-24 16:39:09 +0900

    UI design 추가

--- src/views/authentication/SignIn.vue
+++ src/views/authentication/SignIn.vue
@@ -1,21 +1,33 @@
 <template>
-  <v-container class="fill-height" app>
+  <v-container
+    class="fill-height"
+    app
+  >
     <v-row justify="center">
       <v-col cols="auto">
         <v-card width="460">
           <v-card-text class="text-center px-12 py-16">
-            <div class="text-h4 font-weight-black mb-13">로그인</div>
+            <div class="text-h4 font-weight-black mb-13">
+              로그인
+            </div>
             <v-text-field
               label="Email"
               clearable
               prepend-icon="mdi-email"
-            ></v-text-field>
+            />
             <v-text-field
               label="Password"
               clearable
               prepend-icon="mdi-lock-outline"
-            ></v-text-field>
-            <v-btn block x-large color="primary mt-6" rounded>로그인</v-btn>
+            />
+            <v-btn
+              block
+              x-large
+              color="primary mt-6"
+              rounded
+            >
+              로그인
+            </v-btn>
           </v-card-text>
         </v-card>
       </v-col>
--- src/views/authentication/SignUp.vue
+++ src/views/authentication/SignUp.vue
@@ -1,33 +1,43 @@
 <template>
-  <v-container class="fill-height" app>
+  <v-container
+    class="fill-height"
+    app
+  >
     <v-row justify="center">
       <v-col cols="auto">
         <v-card width="460">
           <v-card-text class="text-center px-12 py-16">
-            <div class="text-h4 font-weight-black mb-13">회원가입</div>
+            <div class="text-h4 font-weight-black mb-13">
+              회원가입
+            </div>
             <v-text-field
               label="Email"
               clearable
               prepend-icon="mdi-email"
-            ></v-text-field>
+            />
             <v-text-field
               label="Name"
               clearable
               prepend-icon="mdi-face"
-            ></v-text-field>
+            />
             <v-text-field
               label="Password"
               clearable
               prepend-icon="mdi-lock-outline"
-            ></v-text-field>
+            />
             <v-text-field
               label="Password-Check"
               clearable
               prepend-icon="mdi-lock-outline"
-            ></v-text-field>
-            <v-btn block x-large color="deep-purple lighten-3 mt-6" rounded
-              >회원가입</v-btn
+            />
+            <v-btn
+              block
+              x-large
+              color="deep-purple lighten-3 mt-6"
+              rounded
             >
+              회원가입
+            </v-btn>
           </v-card-text>
         </v-card>
       </v-col>
